b2c1379d6971e535928411f931130470e72cd3df
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-26 16:02:20 +0200

    Introduce failing test in ex1

--- exercises/run_exercise1_tests.sh
+++ exercises/run_exercise1_tests.sh
@@ -18,6 +18,12 @@ $PATH_TO_EXERCISE1 10 10 band_matrix cg_solve
 #Symmetric band matrix storage tests
 #TO BE ADDED AS PART OF FMW OOP EXERCICE
 
+echo "*************************"
+echo "Sym Band matrix storage tests"
+echo "*************************"
+$PATH_TO_EXERCISE1 10 10 sym_band_matrix dir_solve
+$PATH_TO_EXERCISE1 10 10 sym_band_matrix cg_solve
+
 #Sparse matrix storage tests
 echo "***************************"
 echo "Sparse matrix storage tests"
