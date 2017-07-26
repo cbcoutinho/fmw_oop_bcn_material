e1b517b3b2171132353dbdc9f8eb8a01a09e0831
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-26 17:27:31 +0200

    Some initial work on ex2

--- exercises/exercise2/matrix_factory.f90
+++ exercises/exercise2/matrix_factory.f90
@@ -11,11 +11,12 @@ module matrix_factory_mod
 
   character(*), parameter :: FULL_MATRIX   = "full_matrix"
   character(*), parameter :: BAND_MATRIX   = "band_matrix"
+  character(*), parameter :: SYM_BAND_MATRIX   = "sym_band_matrix"
   character(*), parameter :: SPARSE_MATRIX = "sparse_matrix"
-  character(*), parameter :: MATRIX_TYPE_ERROR_MSG = "matrix_type MUST BE either [full_matrix|band_matrix|sparse_matrix]"
+  character(*), parameter :: MATRIX_TYPE_ERROR_MSG = "matrix_type MUST BE either [full_matrix|band_matrix|sym_band_matrix|sparse_matrix]"
 
   public :: matrix_factory
-  public :: FULL_MATRIX, BAND_MATRIX, SPARSE_MATRIX
+  public :: FULL_MATRIX, BAND_MATRIX, SYM_BAND_MATRIX, SPARSE_MATRIX
 contains
 
   subroutine matrix_factory( matrix_type, matrix )
@@ -27,13 +28,15 @@ contains
       call matrix%free(); deallocate(matrix)
     end if
 
-    mcheck(trim(matrix_type) == FULL_MATRIX .or. trim(matrix_type) == BAND_MATRIX .or. trim(matrix_type) == SPARSE_MATRIX, MATRIX_TYPE_ERROR_MSG)
+    mcheck(trim(matrix_type) == FULL_MATRIX .or. trim(matrix_type) == BAND_MATRIX .or. trim(matrix_type) == SYM_BAND_MATRIX .or. trim(matrix_type) == SPARSE_MATRIX, MATRIX_TYPE_ERROR_MSG)
 
     select case ( trim(matrix_type) )
     case (FULL_MATRIX)
       allocate( dense_matrix_t :: matrix )
     case (BAND_MATRIX)
       allocate( band_matrix_t :: matrix )
+    case (SYM_BAND_MATRIX)
+      allocate( sym_band_matrix_t :: matrix )
     case (SPARSE_MATRIX)
       allocate( sparse_matrix_t :: matrix )
     end select
--- exercises/run_exercise2_tests.sh
+++ exercises/run_exercise2_tests.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-PATH_TO_EXERCISE2=exercise2/build/bin/exercise2
+PATH_TO_EXERCISE1=exercise2/build/bin/exercise2
 
 #Full matrix storage tests
 echo "*************************"
 echo "Full matrix storage tests"
 echo "*************************"
-$PATH_TO_EXERCISE2 10 10 full_matrix dir_solve
-$PATH_TO_EXERCISE2 10 10 full_matrix cg_solve
+time $PATH_TO_EXERCISE1 10 10 full_matrix dir_solve
+time $PATH_TO_EXERCISE1 10 10 full_matrix cg_solve
 
 #Band matrix storage tests
 echo "*************************"
 echo "Band matrix storage tests"
 echo "*************************"
-$PATH_TO_EXERCISE2 10 10 band_matrix dir_solve
-$PATH_TO_EXERCISE2 10 10 band_matrix cg_solve
+time $PATH_TO_EXERCISE1 10 10 band_matrix dir_solve
+time $PATH_TO_EXERCISE1 10 10 band_matrix cg_solve
 
 #Symmetric band matrix storage tests
 #TO BE ADDED AS PART OF FMW OOP EXERCICE
+
+echo "*************************"
+echo "Sym Band matrix storage tests"
+echo "*************************"
+time $PATH_TO_EXERCISE1 10 10 sym_band_matrix dir_solve
+time $PATH_TO_EXERCISE1 10 10 sym_band_matrix cg_solve
 
 #Sparse matrix storage tests
 echo "***************************"
 echo "Sparse matrix storage tests"
 echo "***************************"
-$PATH_TO_EXERCISE2 10 10 sparse_matrix cg_solve
+time $PATH_TO_EXERCISE1 10 10 sparse_matrix cg_solve
